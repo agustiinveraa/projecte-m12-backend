d61e688a9be2087880f13eb51aa9b14a754a5755
Agus <averafaus05@gmail.com>
2024-10-30 21:07:27 +0100

    testing: api dni added

--- index.js
+++ index.js
@@ -1,89 +1,128 @@
-import express from 'express'
-import { PORT, SECRET_JWT_KEY, CONNECTION } from './config.js'
-import jwt from 'jsonwebtoken'
-import { UserRepository } from './user-repository.js'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import { PORT, SECRET_JWT_KEY, CONNECTION } from "./config.js";
+import jwt from "jsonwebtoken";
+import { UserRepository } from "./user-repository.js";
+import cookieParser from "cookie-parser";
+import axios from "axios";
+import FormData from "form-data";
+import fs from "fs";
 
-const app = express()
+const app = express();
 
 CONNECTION.connect(function (err) {
   if (err) {
-    console.error('error connecting: ' + err.stack)
-    return
+    console.error("error connecting: " + err.stack);
+    return;
   }
-  console.log('connected as id ' + CONNECTION.threadId)
-})
+  console.log("connected as id " + CONNECTION.threadId);
+});
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
+app.use(express.json()); // middleware para tratar las siguientes peticiones para req.body
+app.use(cookieParser());
 
-app.use(express.json()) // middleware para tratar las siguientes peticiones para req.body
-app.use(cookieParser())
-
-app.use((req, res, next) => { // middleware para verificar el token en cada peticion
-  const token = req.cookies.access_token
-  req.session = { user: null }
+app.use((req, res, next) => {
+  // middleware para verificar el token en cada peticion
+  const token = req.cookies.access_token;
+  req.session = { user: null };
   try {
-    const data = jwt.verify(token, SECRET_JWT_KEY)
-    req.session.user = data
+    const data = jwt.verify(token, SECRET_JWT_KEY);
+    req.session.user = data;
   } catch {}
-  next() // para que continue con la siguiente peticion
-})
-
-app.get('/formulario', (req, res) => {
-  res.render('formulario');
+  next(); // para que continue con la siguiente peticion
 });
 
+app.get("/formulario", (req, res) => {
+  res.render("formulario");
+});
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const user = req.session.user || {};
-  res.render('prueba', { user });
+  res.render("prueba", { user });
 });
 
-app.post('/login', async (req, res) => {
-  const { nickname, password } = req.body
+app.post("/login", async (req, res) => {
+  const { nickname, password } = req.body;
 
   if (!nickname || !password) {
-    return res.status(400).send('Nickname and password are required');
+    return res.status(400).send("Nickname and password are required");
   }
 
   try {
-    const user = await UserRepository.login({ nickname, password })
-    const token = jwt.sign(user, SECRET_JWT_KEY, { expiresIn: '1h' })
+    const user = await UserRepository.login({ nickname, password });
+    const token = jwt.sign(user, SECRET_JWT_KEY, { expiresIn: "1h" });
     res
-      .cookie('access_token', token, {
-        httpOnly: true, 
-        secure: process.env.NODE_ENV === 'production', // todo: cambiarlo a true  
-        sameSite: 'strict', 
-        maxAge: 1000 * 60 * 60 
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // todo: cambiarlo a true
+        sameSite: "strict",
+        maxAge: 1000 * 60 * 60,
       })
-      .send({ user })
+      .send({ user });
   } catch (error) {
-    res.status(401).send(error.message)
+    res.status(401).send(error.message);
   }
-})
+});
 
-app.post('/register', async (req, res) => {
-  const { dni, nickname, password, name, surname, birthdate } = req.body
+app.post("/register", async (req, res) => {
+  const { dni, nickname, password, name, surname, birthdate } = req.body;
   try {
-    const id = await UserRepository.create({ dni, nickname, password, name, surname, birthdate })
-    res.send('user created')
+    const id = await UserRepository.create({
+      dni,
+      nickname,
+      password,
+      name,
+      surname,
+      birthdate,
+    });
+    res.send("user created");
   } catch (error) {
     // NORMALMENTE POR SEGURIDAD NO SE DEBE DE HACER ASI (se suele hacer con condicional)
-    res.status(400).send(error.message)
+    res.status(400).send(error.message);
   }
-})
+});
 
-app.post('/logout', (req, res) => {
+app.post("/logout", (req, res) => {
   req.session.user = null;
-  res.clearCookie('access_token').send('logout')
-})
+  res.clearCookie("access_token").send("logout");
+});
 
-app.get('/protected', (req, res) => {
+app.get("/protected", (req, res) => {
   const { user } = req.session;
-  if (!user) return res.status(403).send('access denied');
-  res.render('protected', { user }); // Pasa user como un objeto
+  if (!user) return res.status(403).send("access denied");
+  res.render("protected", { user }); // Pasa user como un objeto
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Validar dni x foto API
+
+let data = new FormData();
+// También puede enviar una sola imagen con ambas caras
+data.append("files", fs.createReadStream("./DNI-falso.jpg"));
+// data.append('files', fs.createReadStream('/Users/demo/reverso.jpeg'));
+
+let config = {
+  method: "post",
+  maxBodyLength: Infinity,
+  url: "https://api.wheelz-app.es/dni/process",
+  headers: {
+    "x-api-key": "BdoATJhnAGQa5Bx73jH1Kk6unVLv4AkroZgvgCI5f5U",
+    ...data.getHeaders(),
+  },
+  data: data,
+};
+
+axios
+  .request(config)
+  .then((response) => {
+    const data = response.data;
+    // Extraer y mostrar la fecha de nacimiento
+    const birthDate = data.birthDate;
+    console.log(`Fecha de nacimiento: ${birthDate}`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- user-repository.js
+++ user-repository.js
@@ -1,32 +1,38 @@
-import bcrpyt from 'bcrypt'
-import { CONNECTION, SALT_ROUNDS } from './config.js'
+import bcrpyt from "bcrypt";
+import { CONNECTION, SALT_ROUNDS } from "./config.js";
 
 export class UserRepository {
   static async create({ dni, nickname, password, name, surname, birthdate }) {
-    Validation.dni(dni)
-    Validation.nickname(nickname)
-    Validation.password(password)
-    Validation.name(name)
-    Validation.surname(surname)
-    Validation.birthdate(birthdate)
+    Validation.dni(dni);
+    Validation.nickname(nickname);
+    Validation.password(password);
+    Validation.name(name);
+    Validation.surname(surname);
+    Validation.birthdate(birthdate);
 
-    const [existingUser] = await CONNECTION.promise().query('SELECT * FROM user WHERE nickname = ?', [nickname]);
-    if (existingUser.length > 0) throw new Error('nickname already exists');
+    const [existingUser] = await CONNECTION.promise().query(
+      "SELECT * FROM user WHERE nickname = ?",
+      [nickname]
+    );
+    if (existingUser.length > 0) throw new Error("nickname already exists");
 
     const hashedPassword = await bcrpyt.hash(password, SALT_ROUNDS);
 
     const [result] = await CONNECTION.promise().query(
-      'INSERT INTO user (dni, nickname, password, name, surname, birthdate) VALUES (?, ?, ?, ?, ?, ?)',
+      "INSERT INTO user (dni, nickname, password, name, surname, birthdate) VALUES (?, ?, ?, ?, ?, ?)",
       [dni, nickname, hashedPassword, name, surname, birthdate]
     );
   }
 
-  static async login ({ nickname, password }) {
-    Validation.nickname(nickname)
-    Validation.password(password)
+  static async login({ nickname, password }) {
+    Validation.nickname(nickname);
+    Validation.password(password);
 
-    const [existingUser] = await CONNECTION.promise().query('SELECT * FROM user WHERE nickname = ?', [nickname]);
-    if (existingUser.length === 0) throw new Error('nickname already exists');
+    const [existingUser] = await CONNECTION.promise().query(
+      "SELECT * FROM user WHERE nickname = ?",
+      [nickname]
+    );
+    if (existingUser.length === 0) throw new Error("nickname already exists");
 
     const isValid = await bcrpyt.compare(password, existingUser[0].password);
     if (!isValid) throw new Error("password doesn't match");
@@ -35,30 +41,36 @@ export class UserRepository {
   }
 }
 
-class Validation { // TODO: validaciones completas de los campos
+class Validation {
+  // TODO: validaciones completas de los campos
   static dni(dni) {
     if (!/^\d{8}[A-Z]$/.test(dni) || dni.length !== 9) {
-      throw new Error('dni must contain exactly 8 digits followed by a letter');
+      throw new Error("dni must contain exactly 8 digits followed by a letter");
     }
   }
-  static nickname (nickname) {
-    if (typeof nickname !== 'string') throw new Error('nickname must be a string')
-    if (nickname.length < 3) throw new Error('nickname min 3 length')
+  static nickname(nickname) {
+    if (typeof nickname !== "string")
+      throw new Error("nickname must be a string");
+    if (nickname.length < 3) throw new Error("nickname min 3 length");
   }
-  static password (password) {
-    if (typeof password !== 'string') throw new Error('password must be a string')
-    if (password.length < 8) throw new Error('password min 8 length')
+  static password(password) {
+    if (typeof password !== "string")
+      throw new Error("password must be a string");
+    if (password.length < 8) throw new Error("password min 8 length");
   }
-  static name (name) {
-    if (typeof name !== 'string') throw new Error('name must be a string')
-    if (name.length < 3) throw new Error('name min 3 length')
+  static name(name) {
+    if (typeof name !== "string") throw new Error("name must be a string");
+    if (name.length < 3) throw new Error("name min 3 length");
   }
-  static surname (surname) {
-    if (typeof surname !== 'string') throw new Error('surname must be a string')
-    if (surname.length < 3) throw new Error('surname min 3 length')
+  static surname(surname) {
+    if (typeof surname !== "string")
+      throw new Error("surname must be a string");
+    if (surname.length < 3) throw new Error("surname min 3 length");
   }
-  static birthdate (birthdate) {
-    if (typeof birthdate !== 'string') throw new Error('birthdate must be a string')
-    if (!/^\d{4}-\d{2}-\d{2}$/.test(birthdate)) throw new Error('birthdate must be in format YYYY-MM-DD')
+  static birthdate(birthdate) {
+    if (typeof birthdate !== "string")
+      throw new Error("birthdate must be a string");
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(birthdate))
+      throw new Error("birthdate must be in format YYYY-MM-DD");
   }
 }
